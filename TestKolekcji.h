31ed841790cacb170dc99ae73014bcb7c39a614e
Andrzej Nowik <andrzej@mat.ug.edu.pl>
2020-11-10 14:31:22 +0100

    zadanie z wskaznikow na funkcje i lambda funkcji

--- TestKolekcji.h
+++ TestKolekcji.h
@@ -2,6 +2,17 @@
 #include "define.h"
 class TestKolekcji
 {
+	/*
+	* Zadanie:
+	* 1. zdefiniwaæ typ funkcyjny TFunkcjaFormatowanie
+	* 2. zadeklarowaæ prywatne pole o rzeczonym typie w tej klasie
+	* 3. utorzyæ setter do tego pola
+	* 4. zaimplementowaæ pole w metodzie formatowanie
+	* 5. Stworzyæ lambda funkcjê formatuj¹c¹ (w jakikolwiek sposób)
+	*    i zastosowaæ j¹ w obiekcie klasy TestKolekcji.
+	  uwaga: zmiany wprowadzaæ tak by nie niszczyæ
+	  dzia³aj¹cego ju¿ kodu!
+	*/
 private:
 	vector<string> vec;
 public:
